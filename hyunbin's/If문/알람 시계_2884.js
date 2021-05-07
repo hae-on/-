734e49a780b77e0c5c16fe4e1c219d755b3451d7
hyunbin1 <kimgusqls1@gmail.com>
2021-05-07 14:41:27 +0900

    while문 - 21/05/07

--- hyunbin's/If문/알람 시계_2884.js
+++ hyunbin's/If문/알람 시계_2884.js
@@ -4,5 +4,22 @@
 const sample = `10 10`;
 const input = sample.split(" ");
 
+let H = Number(input[0])
+let M = Number(input[1])
+
+if(M<45;)
+
+
+// 현재 시간보다 45분 빨리 알람 설정하기
+// point: 분이 45분 이하일 경우 시간에서 1을 빼야된다. 
+
 if()
+
+
+
+
+
+
+
+
 
---
+++ hyunbin's/while문/더하기 사이클_1110.js
@@ -0,0 +1,16 @@
+// let input = require('fs').readFileSync('/dev/stdin').toString().split('');
+
+let N= `26`
+let input = N.split("");
+let sum = Number(N[0])+Number(N[1])
+N[1]
+//sum이 두자리 수일때
+if(100 > sum >= 10){
+
+} //sum이 한자리 수일때
+else(sum<10){}
+
+
+console.log(input[0],input[1])
+console.log(sum)
+
